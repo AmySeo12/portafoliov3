3494a040bdcb75d3497ee75c1f23dfe132e1885b
AmySeo12 <maryori.amy@gmail.com>
2016-11-23 07:16:57 -0500

    subiendo habilidades y slider

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+var barra= document.getElementById("barrastyle");
+var barraUno= document.getElementById("barrastyleUno");
+var barraDos= document.getElementById("barrastyleDos");
+var barraTres= document.getElementById("barrastyleTres");
+var barraCuatro= document.getElementById("barrastyleCuatro");
+var barraCinco= document.getElementById("barrastyleCinco");
+
+var barraProgreso= function(){
+	barraUno.value+=5;
+	barra.value += 5;
+	barraDos.value+= 5;
+	barraTres.value +=5;
+	barraCuatro.value +=5;
+	barraCinco.value +=5;
+}
+
 var paperMenu = {
 	$window: $('#paper-window'),
 	$paperFront: $('#paper-front'),
@@ -66,3 +82,56 @@ paperMenu.init();
 	}, 1500);
 	
 })();
+
+//current position
+var pos = 0;
+//number of slides
+var totalSlides = $('#slider-wrap ul li').length;
+//get the slide width
+var sliderWidth = $('#slider-wrap').width();
+
+
+$(document).ready(function(){
+	
+	setInterval(barraProgreso, 100);
+	
+	/*****************
+	 BUILD THE SLIDER
+	*****************/
+	//set width to be 'x' times the number of slides
+	$('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
+	/*************************
+	 //*> OPTIONAL SETTINGS
+	************************/
+	//automatic slider
+	var autoSlider = setInterval(slideRight, 3000);
+	
+	//for each slide 
+	$.each($('#slider-wrap ul li'), function() { 
+	   //set its color
+	   var c = $(this).attr("data-color");
+	   $(this).css("background",c);	   
+	});
+	//hide/show controls/btns when hover
+	//pause automatic slide when hover
+	$('#slider-wrap').hover(
+	  function(){ $(this).addClass('active'); clearInterval(autoSlider); }, 
+	  function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
+	);
+});//DOCUMENT READY
+/***********
+ SLIDE LEFT
+************/
+function slideLeft(){
+	pos--;
+	if(pos==-1){ pos = totalSlides-1; }
+	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 	
+}
+/************
+ SLIDE RIGHT
+*************/
+function slideRight(){
+	pos++;
+	if(pos==totalSlides){ pos = 0; }
+	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 
+}
