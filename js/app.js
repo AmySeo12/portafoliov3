ec0da867726d4f22becef1e349cd0d18b3c9b7da
AmySeo12 <maryori.amy@gmail.com>
2016-11-25 01:57:16 -0500

    subiendo casi ultimos toques

--- js/app.js
+++ js/app.js
@@ -69,69 +69,12 @@ var paperMenu = {
 
 paperMenu.init();
 
-(function(){
-	'use strict';
-	var bar = document.querySelectorAll('.bar-progress');
-	var index;
-
-	setTimeout(function(){
-		for(index = 0; index < bar.length; index++){
-			var atributos = bar[index].getAttribute('data-total');
-			bar[index].style.width = atributos + "%";				
-		}
-	}, 1500);
-	
-})();
-
-//current position
-var pos = 0;
-//number of slides
-var totalSlides = $('#slider-wrap ul li').length;
-//get the slide width
-var sliderWidth = $('#slider-wrap').width();
-
-
 $(document).ready(function(){
 	
 	setInterval(barraProgreso, 100);
-	
-	/*****************
-	 BUILD THE SLIDER
-	*****************/
-	//set width to be 'x' times the number of slides
-	$('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
-	/*************************
-	 //*> OPTIONAL SETTINGS
-	************************/
-	//automatic slider
-	var autoSlider = setInterval(slideRight, 3000);
-	
-	//for each slide 
-	$.each($('#slider-wrap ul li'), function() { 
-	   //set its color
-	   var c = $(this).attr("data-color");
-	   $(this).css("background",c);	   
-	});
-	//hide/show controls/btns when hover
-	//pause automatic slide when hover
-	$('#slider-wrap').hover(
-	  function(){ $(this).addClass('active'); clearInterval(autoSlider); }, 
-	  function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
+	$(".hover").mouseleave(
+		function () {
+			$(this).removeClass("hover");
+		}
 	);
-});//DOCUMENT READY
-/***********
- SLIDE LEFT
-************/
-function slideLeft(){
-	pos--;
-	if(pos==-1){ pos = totalSlides-1; }
-	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 	
-}
-/************
- SLIDE RIGHT
-*************/
-function slideRight(){
-	pos++;
-	if(pos==totalSlides){ pos = 0; }
-	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 
-}
+});
